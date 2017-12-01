e3b803caa9552ab50c76eeec24f4c608409e148a
Raineer <delaritaraineer81@gmail.com>
2017-12-01 14:22:02 +0800

    todo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoInputComponent } from './componets/todo-input/todo-input.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule
