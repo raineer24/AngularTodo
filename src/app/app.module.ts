91b6439fecfeb2368aee98ed3f37649a08771042
Raineer <delaritaraineer81@gmail.com>
2017-12-01 15:22:52 +0800

    TodoService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoInputComponent } from './componets/todo-input/todo-input.component';
+import { TodoService } from './services/todo.service';
 
 
 @NgModule({
@@ -18,7 +19,9 @@ import { TodoInputComponent } from './componets/todo-input/todo-input.component'
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
